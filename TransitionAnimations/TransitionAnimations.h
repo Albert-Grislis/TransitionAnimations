f21744ea1c842da08380f61fce69c86f13008fb1
Albert Grislis <albert.grislis@icloud.com>
2021-01-11 19:32:23 +0300

    Initial Commit

---
+++ TransitionAnimations/TransitionAnimations.h
@@ -0,0 +1,18 @@
+//
+//  TransitionAnimations.h
+//  TransitionAnimations
+//
+//  Created by Albert Grislis on 11.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TransitionAnimations.
+FOUNDATION_EXPORT double TransitionAnimationsVersionNumber;
+
+//! Project version string for TransitionAnimations.
+FOUNDATION_EXPORT const unsigned char TransitionAnimationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransitionAnimations/PublicHeader.h>
+
+
